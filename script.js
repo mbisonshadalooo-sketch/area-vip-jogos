6d26ed494c65d5ea8b438e015d47bfdc0ee90e23
mbisonshadalooo-sketch <mbisonshadalooo@gmail.com>
2026-02-05 15:24:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,9 @@ function login() {
   const usuario = document.getElementById("usuario").value;
   const senha = document.getElementById("senha").value;
 
-  // USUÁRIO E SENHA (você pode mudar depois)
-  const usuarioCorreto = "admin";
-  const senhaCorreta = "1234";
-
-  if (usuario === usuarioCorreto && senha === senhaCorreta) {
+  if (usuario === "admin" && senha === "1234") {
     window.location.href = "jogos.html";
   } else {
-    alert("Usuário ou senha incorretos!");
+    alert("Usuário ou senha inválidos");
   }
 }
