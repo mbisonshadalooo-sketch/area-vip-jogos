434ea6a10ea9f85115ccc9a60169e7f59398f53e
mbisonshadalooo-sketch <mbisonshadalooo@gmail.com>
2026-02-06 13:57:31 -0300

    Update jogos.js

--- js/jogos.js
+++ js/jogos.js
@@ -1,5 +1,5 @@
 const jogos = {
-  ps3: [
+  "PlayStation 3": [
     {
       nome: "Jogo PS3 Exemplo",
       descricao: "Descrição do jogo PS3.",
@@ -8,7 +8,7 @@ const jogos = {
     }
   ],
 
-  ps2: [
+  "PlayStation 2": [
     {
       nome: "Jogo PS2 Exemplo",
       descricao: "Descrição do jogo PS2.",
@@ -17,7 +17,7 @@ const jogos = {
     }
   ],
 
-  ps1: [
+  "PlayStation 1": [
     {
       nome: "Jogo PS1 Exemplo",
       descricao: "Descrição do jogo PS1.",
@@ -26,12 +26,40 @@ const jogos = {
     }
   ],
 
-  snes: [
+  "Super Nintendo": [
     {
-      nome: "Super Mario World",
+      nome: "Jogo SNES Exemplo",
       descricao: "Clássico do Super Nintendo.",
       imagem: "img/WhatsApp Image 2026-02-06 at 11.32.47.jpeg",
       link: "#"
     }
   ]
 };
+
+function carregarJogos() {
+  const container = document.getElementById("conteudo");
+  container.innerHTML = "";
+
+  for (const categoria in jogos) {
+    const titulo = document.createElement("h2");
+    titulo.className = "categoria";
+    titulo.innerText = categoria;
+
+    const lista = document.createElement("div");
+    lista.className = "jogos";
+
+    jogos[categoria].forEach(jogo => {
+      lista.innerHTML += `
+        <div class="jogo">
+          <img src="${jogo.imagem}">
+          <h3>${jogo.nome}</h3>
+          <p>${jogo.descricao}</p>
+          <a href="${jogo.link}" class="btn" download>Download</a>
+        </div>
+      `;
+    });
+
+    container.appendChild(titulo);
+    container.appendChild(lista);
+  }
+}
