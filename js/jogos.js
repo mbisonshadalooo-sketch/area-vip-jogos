cd20b3c28014b4fa3e4a64567920ba47786c517c
mbisonshadalooo-sketch <mbisonshadalooo@gmail.com>
2026-02-06 12:41:33 -0300

    Update jogos.js

---
+++ js/jogos.js
@@ -0,0 +1,37 @@
+const jogos = {
+  ps3: [
+    {
+      nome: "Jogo PS3 Exemplo",
+      descricao: "Descrição do jogo PS3.",
+      imagem: "img/WhatsApp Image 2026-02-06 at 11.32.47.jpeg",
+      link: "#"
+    }
+  ],
+
+  ps2: [
+    {
+      nome: "Jogo PS2 Exemplo",
+      descricao: "Descrição do jogo PS2.",
+      imagem: "img/WhatsApp Image 2026-02-06 at 11.32.47.jpeg",
+      link: "#"
+    }
+  ],
+
+  ps1: [
+    {
+      nome: "Jogo PS1 Exemplo",
+      descricao: "Descrição do jogo PS1.",
+      imagem: "img/WhatsApp Image 2026-02-06 at 11.32.47.jpeg",
+      link: "#"
+    }
+  ],
+
+  snes: [
+    {
+      nome: "Super Mario World",
+      descricao: "Clássico do Super Nintendo.",
+      imagem: "img/WhatsApp Image 2026-02-06 at 11.32.47.jpeg",
+      link: "#"
+    }
+  ]
+};
